a5cd55d693f5d2ecfd211e8da2445ca70d381f81
TimurAztec <timotiaztec@gmail.com>
2024-05-04 14:41:57 +0300

    Added soundeffect settings

--- AmbientPlayer/utils/soundeffectsettings.h
+++ AmbientPlayer/utils/soundeffectsettings.h
@@ -12,13 +12,16 @@ public:
 
     double interval() const { return _interval; }
     bool shouldPlayOnce() const { return _playOnce; }
+    int defaultVolume() const { return _defaultVolume; }
 
     void setInterval(const double& interval) { _interval = interval; }
     void setShouldPlayOnce(const bool& playOnce) { _playOnce = playOnce; }
+    void setDefaultVolume(const bool& defaultVolume) { _defaultVolume = defaultVolume; }
 
 private:
     double _interval;
     bool _playOnce;
+    int _defaultVolume;
 };
 
 #endif // SOUNDEFFECTSETTINGS_H
